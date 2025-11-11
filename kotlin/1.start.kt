3f11388be9c0eb32b4e6be48212a4a9bc5760bf5
chapi1420 <nadewnahom8@gmail.com>
2025-11-11 15:49:47 +0300

    start learning on android development

---
+++ kotlin/1.start.kt
@@ -0,0 +1,4 @@
+fun main() {
+    println("hi there")
+    println("I can tell that kotlin is the slowest programming language I have ever seen\n Yeah vs code is the slowest way to write Kotlin as it is not initially built to run programming languages like this. \n If the same code here was run on intellijIdea, it  would have been much faster than you think")
+}
